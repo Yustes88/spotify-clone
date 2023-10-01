d5b5d10c43e35a3e31503624a4bf8b426dfb0630
Yulya <iuliiaobr.io@gmail.com>
2023-10-01 14:21:43 +0700

    Add sidebar items

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useMemo } from "react";
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi'
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 type SidebarProps = {
   children: React.ReactNode;
@@ -22,7 +23,7 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
     },
     {
       icon: BiSearch,
-      lable: 'Search',
+      label: 'Search',
       active: pathname === '/search',
       href: '/search'
     }
@@ -40,7 +41,20 @@ const Sidebar: React.FC<SidebarProps> = ({children}) => {
       p-2
       ">
         <Box>
-          Sidebar navigation
+          <div className="
+          flex
+          flex-col
+          gap-y-4
+          px-5
+          py-4
+          ">
+            {routes.map((item) => (
+              <SidebarItem
+              key={item.label}
+              {...item}
+              />
+            ))}
+          </div>
         </Box>
         <Box className="overflow-y-auto h-full">
           Song library
