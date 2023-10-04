27c51c693ceb9929dfbbb24c30aaa0d25a8d62ff
Yulya <iuliiaobr.io@gmail.com>
2023-10-04 18:12:07 +0700

    installs supabase helpers

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Database } from "@/types_db"
+
+type SupabaseProviderProps = {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
+  children
+}) => {
+  
+}
