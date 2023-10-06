c3df5f84be41a69bc3fc99fef330e95b79993042
Yulya <iuliiaobr.io@gmail.com>
2023-10-06 18:03:15 +0700

    adds upload modal component

---
+++ components/UploadModal.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import Modal from './Modal'
+import useUploadModal from '@/hooks/useUploadModal'
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal()
+
+  const onChange = (open: boolean) => {
+    if(!open) {
+      uploadModal.onClose()
+    }
+  }
+
+  return (
+    <Modal title='Upload modal title' description='upload modal description' isOpen={uploadModal.isOpen} onChange={onChange}>
+      Upload modal
+    </Modal>
+  )
+}
+
+export default UploadModal
