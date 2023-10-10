d1687d2ee2e2aaa7374bbe1573323e8ba0c8874f
Yulya <iuliiaobr.io@gmail.com>
2023-10-10 20:20:06 +0700

    adds player button functionality

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -8,6 +8,8 @@ import {BsPauseFill, BsPlayFill} from 'react-icons/bs'
 import {HiSpeakerXMark, HiSpeakerWave} from 'react-icons/hi2'
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import { useState } from "react";
 
 type PlayerContentProps = {
   song: Song;
@@ -20,9 +22,44 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
   songUrl,
 }) => {
 
-  const Icon = true ? BsPauseFill : BsPlayFill;
-  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false)
 
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if(player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if(!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong)
+
+  }
+
+  const onPlayPrevios = () => {
+    if(player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const previousSong = player.ids[currentIndex - 1];
+
+    if(!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(previousSong)
+
+  }
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
@@ -37,11 +74,11 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         </div>
       </div>
       <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
-        <AiFillStepBackward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick={() => {}}/>
+        <AiFillStepBackward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick={onPlayPrevios}/>
         <div onClick={() => {}} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
           <Icon size={30} className = 'text-black'/>
         </div>
-        <AiFillStepForward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick = {() => {}}/>
+        <AiFillStepForward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick = {onPlayNext}/>
       </div>
       <div className="hidden md:flex w-full justify-end pr-2">
         <div className="flex items-center gap-x-2 w-[120px]">
