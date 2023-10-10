82985ea449f63bb221fdb01cd867bbdb4ffdb1ad
Yulya <iuliiaobr.io@gmail.com>
2023-10-10 19:04:27 +0700

    creates song url hook

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,14 +1,24 @@
 'use client'
 
+import useLoadSongUrl from "@/hooks/UseLoadSongUrl"
+import useGetSongById from "@/hooks/useGetSongById"
 import usePlayer from "@/hooks/usePlayer"
 
 
 const Player = () => {
   const player = usePlayer()
+  const {song} = useGetSongById(player.activeId)
+
+  const songUrl = useLoadSongUrl(song!)
+
+  if(!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
   
 
   return (
-    <div>Player</div>
+    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">Player</div>
   )
 }
 
