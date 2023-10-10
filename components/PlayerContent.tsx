5abc3a0b9840507144d9d53026d1eb83779cca44
Yulya <iuliiaobr.io@gmail.com>
2023-10-10 20:43:26 +0700

    fix pause error

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -106,13 +106,13 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         </div>
       </div>
       <div className="flex md:hidden col-auto w-full justify-end items-center">
-        <div className="h-10 w-10 felx items-center justify-center rounded-full bg-white p-1 cursor-pointer" onClick={() => {}}>
+        <div className="h-10 w-10 felx items-center justify-center rounded-full bg-white p-1 cursor-pointer" onClick={handlePlay}>
           <Icon size={30} className='text-black'/>
         </div>
       </div>
       <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
         <AiFillStepBackward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick={onPlayPrevios}/>
-        <div onClick={() => {}} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+        <div onClick={handlePlay} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
           <Icon size={30} className = 'text-black'/>
         </div>
         <AiFillStepForward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick = {onPlayNext}/>
