cde678feb2a248f92f8e2903caa014b527d45c97
Yulya <iuliiaobr.io@gmail.com>
2023-10-10 19:45:25 +0700

    creates player layout with no functionality

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,6 +4,11 @@ import { Song } from "@/types"
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 
+import {BsPauseFill, BsPlayFill} from 'react-icons/bs'
+import {HiSpeakerXMark, HiSpeakerWave} from 'react-icons/hi2'
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import Slider from "./Slider";
+
 type PlayerContentProps = {
   song: Song;
   songUrl: string;
@@ -15,9 +20,11 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
   songUrl,
 }) => {
 
+  const Icon = true ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
 
   return (
-    <div className="grid grid-cols md:grid-cols-3 h-full">
+    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
       <div className="flex w-full justify-start">
         <div className="flex items-center gap-x-4">
           <MediaItem data={song}/>
@@ -26,7 +33,20 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
       </div>
       <div className="flex md:hidden col-auto w-full justify-end items-center">
         <div className="h-10 w-10 felx items-center justify-center rounded-full bg-white p-1 cursor-pointer" onClick={() => {}}>
-          {/* <Icon/> */}
+          <Icon size={30} className='text-black'/>
+        </div>
+      </div>
+      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
+        <AiFillStepBackward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick={() => {}}/>
+        <div onClick={() => {}} className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer">
+          <Icon size={30} className = 'text-black'/>
+        </div>
+        <AiFillStepForward size={30} className='text-neutral-400 cursor-pointer hover:text-white transition' onClick = {() => {}}/>
+      </div>
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon onClick = {() => {}} size={34} className = 'cursor-pointer'/>
+          <Slider/>
         </div>
       </div>
     </div>
