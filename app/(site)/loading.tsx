996ab9ffb321dafa962eda4eb83a54b53bbd5842
Yulya <iuliiaobr.io@gmail.com>
2023-10-11 18:07:10 +0700

    adds loading without spinner

---
+++ app/(site)/loading.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Box from "@/components/Box"
+
+const Loading = () => {
+  return (
+    <Box className="h-full flex items-center justify-center">
+      
+    </Box>
+  )
+}
+
+export default Loading
